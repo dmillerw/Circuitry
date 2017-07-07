abd04f53c8e78057af998d97ba3d0d086a867865
dmillerw <dmillerw@gmail.com>
2017-07-06 17:42:23 -0700

    Screens! Things work!

--- src/main/java/me/dmillerw/IO/proxy/ClientProxy.java
+++ src/main/java/me/dmillerw/IO/proxy/ClientProxy.java
@@ -1,7 +1,10 @@
 package me.dmillerw.io.proxy;
 
+import me.dmillerw.io.block.tile.TileScreen;
 import me.dmillerw.io.client.model.BaseModelLoader;
+import me.dmillerw.io.client.render.RenderTileScreen;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 /**
@@ -14,5 +17,7 @@ public class ClientProxy extends CommonProxy {
         super.preInit(event);
 
         ModelLoaderRegistry.registerLoader(new BaseModelLoader());
+
+        ClientRegistry.bindTileEntitySpecialRenderer(TileScreen.class, new RenderTileScreen());
     }
 }
