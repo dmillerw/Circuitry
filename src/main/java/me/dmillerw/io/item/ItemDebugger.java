a93fc71a757999b94b0411e6cd0548afe9474abb
dmillerw <dmillerw@gmail.com>
2017-07-11 23:16:39 -0700

    Removed debug code

--- src/main/java/me/dmillerw/io/item/ItemDebugger.java
+++ src/main/java/me/dmillerw/io/item/ItemDebugger.java
@@ -4,10 +4,7 @@ import me.dmillerw.io.block.tile.core.TileToolContainer;
 import me.dmillerw.io.lib.ModInfo;
 import me.dmillerw.io.lib.ModTab;
 import me.dmillerw.io.network.GuiHandler;
-import me.dmillerw.io.network.PacketHandler;
-import me.dmillerw.io.network.packet.client.CUpdatePorts;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumActionResult;
@@ -36,15 +33,6 @@ public class ItemDebugger extends Item {
     public EnumActionResult onItemUseFirst(EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
         TileEntity tile = world.getTileEntity(pos);
         if (tile != null && tile instanceof TileToolContainer) {
-            if (!world.isRemote) {
-                if (player.isSneaking()) {
-                    System.out.println(((TileToolContainer) tile).getGrid().uuid);
-                    System.out.println(((TileToolContainer) tile).getGrid().getMembers().size());
-                    return EnumActionResult.SUCCESS;
-                }
-                PacketHandler.INSTANCE.sendTo(CUpdatePorts.from((TileToolContainer) tile), (EntityPlayerMP) player);
-            }
-
             GuiHandler.Gui.DEBUGGER.openGui(player, pos);
         }
 
